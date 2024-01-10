98a6a4a0c391d32e9031554e3f5c1e3db6af2901
jayesh <jayesh.singh@staqo.com>
2024-01-10 11:55:01 +0530

    useState hook in react

--- learning_react_inerns/useState/UseState.jsx
+++ learning_react_inerns/useState/UseState.jsx
@@ -1,36 +1,39 @@
 import { useState } from 'react';
 import { sculptureList } from './data.js';
+import Counter from './Counter.jsx';
 
 export default function UseStateCom() {
-//   let index = 0;
-let [index,setIndex] = useState(0);
+    //   let index = 0;
+    let [index, setIndex] = useState(0);
 
-  function handleClick() {
-    // index = index + 1;
-    setIndex(index+1)
-    console.log(index, 'this is index');
-  }
+    function handleClick() {
+        // index = index + 1;
+        setIndex(index + 1)
+        console.log(index, 'this is index');
+    }
 
-  let sculpture = sculptureList[index];
-  return (
-    <>
-      <button onClick={handleClick}>
-        Next
-      </button>
-      <h2>
-        <i>{sculpture.name} </i> 
-        by {sculpture.artist}
-      </h2>
-      <h3>  
-        ({index + 1} of {sculptureList.length})
-      </h3>
-      <img 
-        src={sculpture.url} 
-        alt={sculpture.alt}
-      />
-      <p>
-        {sculpture.description}
-      </p>
-    </>
-  );
+    let sculpture = sculptureList[index];
+    return (
+        <>
+            <button onClick={handleClick}>
+                Next
+            </button>
+            <h2>
+                <i>{sculpture.name} </i>
+                by {sculpture.artist}
+            </h2>
+            <h3>
+                ({index + 1} of {sculptureList.length})
+            </h3>
+            <img
+                src={sculpture.url}
+                alt={sculpture.alt}
+            />
+            <p>
+                {sculpture.description}
+            </p>
+            <h1>The below is counter application</h1>
+            <Counter />
+        </>
+    );
 }
